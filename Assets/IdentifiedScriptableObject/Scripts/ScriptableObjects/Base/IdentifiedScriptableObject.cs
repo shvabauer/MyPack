ba3bfcbd0b9198585446ec3cb04f7d35f68150a7
SER-PC <shvabauer92@gmail.com>
2023-07-04 21:47:31 +0700

    IdentifiedScriptableObject

---
+++ Assets/IdentifiedScriptableObject/Scripts/ScriptableObjects/Base/IdentifiedScriptableObject.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+using ModestTree;
+
+namespace MyPack.IdentifiedSO.ScriptableObjects.Base
+{
+    public class ScriptableObjectIdAttribute : PropertyAttribute
+    {
+    }
+
+    public class IdentifiedScriptableObject : ScriptableObject
+    {
+        public Guid Id;
+
+        [ScriptableObjectId]
+        [SerializeField] private string _id = string.Empty;
+
+        private void Awake()
+        {
+            Id = Guid.TryParse(_id, out var id) ? id : Guid.Empty;
+        }
+
+        public void OnValidate()
+        {
+            if (Id == Guid.Empty)
+            {
+                if (_id.IsEmpty())
+                {
+                    ResetId();
+                }
+                else
+                {
+                    Id = Guid.Parse(_id);
+                }
+            }
+        }
+
+        [ContextMenu("ResetId")]
+        private void ResetId()
+        {
+            Id = Guid.NewGuid();
+            _id = Id.ToString();
+        }
+
+        public void Reset()
+        {
+            ResetId();
+        }
+    }
+}
